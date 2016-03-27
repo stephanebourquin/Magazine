8057a9696fd102b10b8608c3e00bfb065a27f334
stephanebourquin <bourquin.stephane@gmail.com>
2016-03-27 13:55:31 +0200

    Create scolastik.js

---
+++ scolastik.js
@@ -0,0 +1,35 @@
+// Initialize app and store it to myApp variable for futher access to its methods
+var myApp = new Framework7();
+ 
+// We need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+ 
+// Add view
+var mainView = myApp.addView('.view-main', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
+});
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+   // loop: true,
+    
+    // If we need pagination
+   // pagination: '.swiper-scrollbar',
+    
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    
+    // And if we need scrollbar
+    scrollbar: '.swiper-scrollbar',
+  })      
+
+$$('.swiper-slide-menu').click(function(e) {
+
+    e.preventDefault();
+    $$(".menu .active").removeClass('active');
+    $$(this).addClass('active');
+    mySwiper.slideTo( $$('#1').index(),100,false );
+});
