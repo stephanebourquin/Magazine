85fa6f3b04d2503dafaa1fa319932808bfeb2ecb
stephanebourquin <bourquin.stephane@gmail.com>
2016-03-30 11:04:57 +0200

    Update scolastik.js

--- scolastik.js
+++ scolastik.js
@@ -9,24 +9,53 @@ var mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
 });
-
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-   // loop: true,
-    
-    // If we need pagination
-   // pagination: '.swiper-scrollbar',
-    
-    // Navigation arrows
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    
-    // And if we need scrollbar
-    scrollbar: '.swiper-scrollbar',
-  })      
-
+var mySwiper ;
+var current_url;
+get_nouveautes();
+function swipe_init () {
+						mySwiper = new Swiper ('.swiper-container', {
+							// Optional parameters
+							direction: 'horizontal',
+						   // loop: true,
+							
+							// If we need pagination
+						   // pagination: '.swiper-scrollbar',
+							
+							// Navigation arrows
+							nextButton: '.swiper-button-next',
+							prevButton: '.swiper-button-prev',
+							
+							// And if we need scrollbar
+							scrollbar: '.swiper-scrollbar',
+						  })      
+						}
 function goto (id) {
     mySwiper.slideTo( $$('#'+id).index(),500,false );
-	
 }
+
+function set_current_url (url) {
+	
+	current_url =url;
+}
+
+function mag_init () {
+	mainView.router.loadPage("magazine.html");
+ var  xmlhttp=new XMLHttpRequest();
+
+xmlhttp.open("GET", current_url,false);
+xmlhttp.send();
+if (xmlhttp) { 
+document.getElementById("magazine").innerHTML=xmlhttp.responseText;
+  swipe_init ();
+}
+}
+
+ function get_nouveautes() {
+var  xmlhttp=new XMLHttpRequest();
+xmlhttp.open("GET","http://mag.art-zoo.com/?ajax=true",false);
+xmlhttp.send();
+document.getElementById("grid_index").innerHTML=xmlhttp.responseText;
+			}
+
+myApp.onPageInit('my_mag', function (page) { mag_init();});
+myApp.onPageInit('index', function (page) { get_nouveautes();});
