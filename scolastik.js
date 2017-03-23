9c2662d6707e161511c2aa3804d5b3c505c03424
stephanebourquin <bourquin.stephane@gmail.com>
2017-03-23 12:31:08 +0100

    Update scolastik.js

--- scolastik.js
+++ scolastik.js
@@ -1,27 +1,4 @@
 function sendsms() {
             alert ("try to sensd message ....");
-var msg = {
-            
-            phoneNumber:"0033651583235",
-            textMessage:"Hello world !!!";
-        };
 
-        sms.sendMessage(msg, function(message) {
-            console.log("success: " + message);
-            navigator.notification.alert(
-                'Message to ' + number + ' has been sent.',
-                null,
-                'Message Sent',
-                'Done'
-            );
-
-        }, function(error) {
-            console.log("error: " + error.code + " " + error.message);
-            navigator.notification.alert(
-                'Sorry, message not sent: ' + error.message,
-                null,
-                'Error',
-                'Done'
-            );
-        });
 }
