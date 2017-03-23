fb9e735e1da6916314a7996f4e5739c90b798f8b
stephanebourquin <bourquin.stephane@gmail.com>
2017-03-23 12:33:57 +0100

    Update scolastik.js

--- scolastik.js
+++ scolastik.js
@@ -1,4 +1,14 @@
 function sendsms() {
             alert ("try to sensd message ....");
+            var msg = {
+            phoneNumber:"33651583235",
+            textMessage:"Hello world !!!";
+        };
+
+        sms.sendMessage(msg, function(message) {
+            alert("success: " + message); 
+        }, function(error) {
+           alert("error: " + error.code + " " + error.message);       
+        });
 
 }
