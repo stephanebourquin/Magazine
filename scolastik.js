23159d6f752df32c4d041480eb4016ac44808b93
stephanebourquin <bourquin.stephane@gmail.com>
2017-03-23 13:24:37 +0100

    Update scolastik.js

--- scolastik.js
+++ scolastik.js
@@ -8,7 +8,7 @@ function sendsms() {
         var options = {
             replaceLineBreaks: false, // true to replace \n by a new line, false by default
             android: {
-                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                intent: ''  // send SMS with the native android SMS messaging
                 //intent: '' // send SMS without open any other app
             }
         };
